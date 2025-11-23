d30e515f4d195289e745315de16cea171ff36300
BASS13013 <hb.service13@hotmail.com>
2025-11-23 11:14:03 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,8 @@
-javascriptconst CACHE_NAME = 'badminton-v1';
+Voici exactement ce qu'il faut copier, à partir de la ligne qui commence par const :
+const CACHE_NAME = 'badminton-v2';
 const urlsToCache = [
-  './',
-  './index.html',
+  '/',
+  '/index.html',
   'https://cdn.tailwindcss.com',
   'https://unpkg.com/react@18/umd/react.production.min.js',
   'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
@@ -9,16 +10,57 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+  console.log('Service Worker installing...');
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Mise en cache des fichiers');
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service Worker activating...');
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Suppression ancien cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
-      .catch(() => caches.match('./index.html'))
+      .then(response => {
+        if (response) {
+          console.log('Fichier trouvé en cache:', event.request.url);
+          return response;
+        }
+        console.log('Téléchargement:', event.request.url);
+        return fetch(event.request)
+          .then(response => {
+            if (!response || response.status !== 200) {
+              return response;
+            }
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME).then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+            return response;
+          });
+      })
+      .catch(() => {
+        console.log('Erreur réseau, utilisation du cache');
+        return caches.match('/index.html');
+      })
   );
 });
